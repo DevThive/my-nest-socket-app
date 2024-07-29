4ae679415507ea0cb5a6f8f6eb57c080f849f3b5
데브티브 <thive8564@gmail.com>
2024-07-29 15:04:44 +0900

    add socket api

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,33 @@
+// src/chat/chat.gateway.ts
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: 'http://localhost:3000', // 클라이언트의 URL
+    methods: ['GET', 'POST'],
+  },
+})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log('New client connected:', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected:', client.id);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: any): void {
+    this.server.emit('message', payload); // 모든 클라이언트에게 메시지 전송
+  }
+}
