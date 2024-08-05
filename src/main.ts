9632429814c3cd0046df02c1f291d1d711166854
DevThive <thive8564@gmail.com>
2024-08-06 01:24:56 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS 및 Referrer-Policy 설정
-  app.enableCors();
-
-  app.use((req, res, next) => {
-    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-    next();
+  // CORS 설정
+  app.enableCors({
+    origin: 'https://random-chat-tau.vercel.app', // 허용할 출처
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+    credentials: true, // 쿠키와 같은 인증 정보를 포함할지 여부
   });
 
   await app.listen(4000);
