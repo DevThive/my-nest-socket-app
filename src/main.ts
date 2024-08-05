9984d0b3dc71b793d763a9a1e0a039805fef75ba
DevThive <thive8564@gmail.com>
2024-08-06 01:18:36 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 활성화
+  app.enableCors();
+
   await app.listen(4000);
 }
 bootstrap();
